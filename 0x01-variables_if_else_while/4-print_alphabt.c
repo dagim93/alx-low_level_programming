f17a4bd7958e93a53ac76141a2ca0dba1444d9bf
dagim93 <dagimtezera9300@gmail.com>
2022-03-11 15:38:11 -0800

    add file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Print the lowercase alphabet in order
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q'){
+			putchar(ch);
+			ch++;
+		}
+		else{
+			ch++;
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
