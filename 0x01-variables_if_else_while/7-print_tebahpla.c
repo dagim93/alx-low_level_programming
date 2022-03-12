eb3fb6e24d9057e750645de9d43740b916fc708d
dagim93 <dagimtezera9300@gmail.com>
2022-03-11 16:01:30 -0800

    add file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Print the lowercase alphabet in order
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%i", a);
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
