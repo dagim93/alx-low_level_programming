cf6cb5641237ce9f0061c1c755fc6d31af1c1112
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-16 01:51:57 +0300

    style edit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,19 +9,19 @@
 
 void jack_bauer(void)
 {
-      int hour_1;
-      int hour_2;
-      int minute_1;
-      int minute_2;
-      int h;
-      
-      hour_1 = 0;
-      while (hour_1 < 3)
-      {
-            if (hour_1 == 2)
-                  h = 4; 
-            else
-                  h = 10;
+int hour_1;
+int hour_2;
+int minute_1;
+int minute_2;
+int h;
+
+hour_1 = 0;
+while (hour_1 < 3)
+{
+      if (hour_1 == 2)
+            h = 4; 
+      else
+            h = 10;
             
             hour_2 = 0;
             while (hour_2 < h)
@@ -40,12 +40,12 @@ void jack_bauer(void)
                               _putchar('\n');
                               minute_2++;
                         }
-                    minute_1++;
+                        minute_1++;
                   }
-              hour_2++;
+                  hour_2++;
             }
-        hour_1++;
-      }
+      hour_1++;
+}
   
   
 }
