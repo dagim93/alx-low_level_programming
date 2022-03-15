70bd92016d8c49ec3d0981a6423b5532651f6da0
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-15 16:05:19 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
 #include "main.h"
-
+/**
+ * print the word _putchar using while loop
+ * 
+ * Return: O.
+ *
+ */
 int main(void)
 {
 	char c[8] = "_putchar";
 	int i = 0;
+	
 	while (i < 9)
 	{
 		_putchar(c[i]);
 		i++;
 	}
-	return 0;
+	_putchar('\n');
+	return (0);
 }
