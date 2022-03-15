a8d12f140cb5c93cb4901f16b9068f637fe01e4c
dagim93 <dagimtezera9300@gmail.com>
2022-03-15 13:13:03 -0700

    add 6 abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*int _abs(int) - gives absolute value of a number
+*
+*Return : int.
+*/
+
+int _abs(int)
+{
+	if (int >= 0)
+	{
+	int =int * 1;
+	}
+	else
+	{
+	int = int * -1;
+	}
+
+	return (int);
+}
