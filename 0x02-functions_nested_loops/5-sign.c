d29091e06d0ac9edc516878bc3f67caa43405a3d
dagim93 <dagimtezera9300@gmail.com>
2022-03-15 12:25:08 -0700

    add 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+	}
+	if else (n == '0')
+	{
+		_putchar('0');
+	}
+	if else (n < 0)
+	{
+		_putchar('-')
+	}
+	return _putchar;
+
+}
