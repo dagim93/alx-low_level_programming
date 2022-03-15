0ba40dbfeca2bb4cdaf74654ce8372a608b3d275
dagim93 <dagimtezera9300@gmail.com>
2022-03-15 07:55:42 -0700

    fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <ctype.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-in _islower(int c);
+int _islower(int c);
