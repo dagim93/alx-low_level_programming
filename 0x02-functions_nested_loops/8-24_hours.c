ffb4ba9597f6e12dc2d5f7eb220c7fa9e13bd0d2
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-16 01:14:57 +0300

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* print_last_digit - tells the value is alphabet or not
+* @last : description
+*
+* Return: 1 if the value is positive. 0 if the value is zero. -1 otherwise
+*/
+
+void jack_bauer(void)
+{
+      int hour_1 = 0;
+      int hour_2 = 0;
+      int minute_1 = 0;
+      int minute_2 = 0;
+      
+      while (hour_1 > 3)
+      {
+            while (hour_2 > 10)
+            {
+                  while (minute_1 > 6)
+                  {
+                        while (minute_2 > 10)
+                        {
+                              _putchar(hour_1 + '0');
+                              _putchar(hour_2 + '0');
+                              _putchar(':')
+                              _putchar(minute_1 + '0');
+                              _purchar(minute_2 + '0');
+                              minute_2++;
+                        }
+                    minute_1++;
+                  }
+              hour_2++;
+            }
+        hour_1++;
+      }
+  
+  
+}
