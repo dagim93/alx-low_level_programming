21dade9b966c2a76c8b4423fee9996a22586ea8d
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-15 19:44:11 +0300

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,11 @@
 
 /*
 * _islower - checks if c is lower alphabet or not
+* @c : character to check the case
 * Return - 0 or 1
 */
 
 int _islower(int c)
 {
-	return (c > 97 && c < 122);	
+	return (c > 97 && c < 122);
 }
