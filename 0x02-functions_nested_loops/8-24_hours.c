73949a6f0d8a0c2d1a25f5c5e1be2bc07f9487bb
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-16 01:54:53 +0300

    edit style

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -23,28 +23,28 @@ while (hour_1 < 3)
       else
             h = 10;
             
-            hour_2 = 0;
-            while (hour_2 < h)
+      hour_2 = 0;
+      while (hour_2 < h)
+      {
+            minute_1 = 0;
+            while (minute_1 < 6)
             {
-                  minute_1 = 0;
-                  while (minute_1 < 6)
+                  minute_2 = 0;
+                  while (minute_2 < 10)
                   {
-                        minute_2 = 0;
-                        while (minute_2 < 10)
-                        {
-                              _putchar('0' + hour_1);
-                              _putchar('0' + hour_2);
-                              _putchar(':');
-                              _putchar('0' + minute_1);
-                              _putchar('0' + minute_2);
-                              _putchar('\n');
-                              minute_2++;
-                        }
-                        minute_1++;
+                        _putchar('0' + hour_1);
+                        _putchar('0' + hour_2);
+                        _putchar(':');
+                        _putchar('0' + minute_1);
+                        _putchar('0' + minute_2);
+                        _putchar('\n');
+                        minute_2++;
                   }
-                  hour_2++;
+                  minute_1++;
             }
-      hour_1++;
+            hour_2++;
+      }
+hour_1++;
 }
   
   
