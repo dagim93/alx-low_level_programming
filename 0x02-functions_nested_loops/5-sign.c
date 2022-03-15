1240f3f144643c6ce290b00999d114802618fa51
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-15 22:38:02 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,18 +2,25 @@
 
 int print_sign(int n)
 {
+	int result;
+	
 	if (n > 0)
 	{
 		_putchar('+');
+		result = 1;
 	}
 	else if (n == '0')
 	{
 		_putchar('0');
+		result = 0;
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
+		result = -1;
 	}
-	return (int _putchar);
+	_putchar(',\n');
+	
+	return (result);
 
 }
