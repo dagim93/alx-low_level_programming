67c8cbfde39603eb3ad9bdd939bb3eb6055fb114
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-16 00:22:03 +0300

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_last_digit - tells the value is alphabet or not
+* @last : description
+*
+* Return: 1 if the value is positive. 0 if the value is zero. -1 otherwise
+*/
+
+int print_last_digit (int last)
+{
+      last_digit = last % 10;
+  
+      return (last_digit);
+}
