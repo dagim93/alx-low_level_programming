6256e873ac8cdf523187d5988d296ddd97b2cbb2
dagim93 <dagimtezera9300@gmail.com>
2022-03-16 10:18:29 -0700

    modified files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 89;
+	positive_or_negative(i);
+
+	return (0);
+}
