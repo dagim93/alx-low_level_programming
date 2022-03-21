e89c86139311eef04870365bcd873327bedfdfb9
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-22 00:43:12 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,7 +7,18 @@
 * Return : nothing
 */
 
+int main(void)
+{
+    char *str;
+    int len;
+
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
+    return (0);
+}
+
 int _strlen(char *s)
 {
-      strlen(*s);
+      strlen((int*)s);
 }
