8f9cea97ee03d5af0264a254d70bbcc154583b7b
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-22 01:04:20 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,16 +3,4 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
