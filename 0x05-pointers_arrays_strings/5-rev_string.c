222031a8281b52a76c0a50bd363270ed3a85b208
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-22 01:47:24 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,12 +11,9 @@ void rev_string(char *s)
       int i = 0;
       
       while (s[i])
-            i++;
-      
-      while (i >= 0)
       {
-            _putchar(s[i]);
-            --i;
+            _putchar(s[-i]);
+            i++;
       }
 _putchar('\n');
 }
