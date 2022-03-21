24d854b3a5959797f9cb8e0715f48a6266708b47
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-22 01:25:37 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,11 @@
 
 void print_rev(char *s)
 {
-      int i = 1;
-      
-      while (s[-i])
-      {
-            _putchar(s);
-            i++;
-      }
+int i = 1;
+while (s[-i])
+{
+	_putchar(s[-i]);
+	i++;
+}
+_putchar('\n');
 }
