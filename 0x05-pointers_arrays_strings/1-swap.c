a95e86a47b9bcb8adefffb45699f6aa2d4622767
dagim93 <dagimtezera9300@gmail.com>
2022-03-21 11:55:40 -0700

    add files

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* swap_int - swap the value of two integers
+* @a : swap value with b
+* @b : swap value with a
+*
+* Return : nothing
+*/
+
+void swap_int(int*a, int*b)
+{
+	*a = 42;
+	*b = 98;
+}
