a039880ba5c4c045162f2f5dee4024760c3b6faa
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-22 01:10:18 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 /**
-*
+* _puts- print the string
 *
 *
 */
 
 void _puts(char *str)
-{
-      _putchar(*str);
+{     
+      int i=0;
+      while (str[i])
+      {
+            _putchar(str[i]);
+            i++;
+      }
 }
