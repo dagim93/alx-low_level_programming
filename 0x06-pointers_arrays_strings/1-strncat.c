7df068807eae33cbfbd9a3c678027ef87ff659a9
dagim93 <dagimtezera9300@gmail.com>
2022-03-23 06:22:32 -0700

    add strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strncat - concatinate two strings by using n bytes for src.
+*
+* Return : char dest.
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i,j;
+
+	for (i = 0; dest[i] != '0'; ++i);
+
+	for (j = 0; j < n; ++j, ++i)
+	{
+		if (dest[j] == '0')
+			break;
+
+		dest[i] = src[j];
+	}
+
+	return dest;
+}
