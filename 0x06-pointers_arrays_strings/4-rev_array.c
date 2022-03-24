848df16ccfeb96ad7b37b24284b4559d548bd8e4
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-24 21:46:09 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,22 +3,21 @@
 /**
 * reverse_array - reverse an array.
 *
-*
 * Return: nothing.
 */
 
 
 void reverse_array(int *a, int n)
 {
-    int i,j,k;
+    int i, j, k;
 
     int b[n];
-    n = n-1;
+    n = n - 1;
 
-    for (i = 0; n >= 0; --n, ++i)
-        b[i]=a[n];
+    for (i = 0; n >= 0; --n, i++)
+        b[i] = a[n];
 
     i = i-0;
-    for (k=0; i >= k;++k)
-        a[k]=b[k];
+    for (k = 0; i >= k; k++)
+        a[k] = b[k];
 }
