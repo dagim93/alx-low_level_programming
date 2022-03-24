2a99372e7468e7a4b3e9e58ae7b8233be6f06134
dagim93 <dagimtezera9300@gmail.com>
2022-03-24 10:19:49 -0700

    add third file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two strings
+* @s1: the first to be compared
+*
+* Retrun : cmp.
+*/
+
+int _strcmp(char *s1, char *s1)
+{
+	int i, j, cmp;
+
+	for (i = 0; s1[i] != '0'; )
+		++i;
+	for (j = 0; s2[j] != '0'; )
+		++j;
+
+	cmp = i - j;
+	return (cmp);
+}
