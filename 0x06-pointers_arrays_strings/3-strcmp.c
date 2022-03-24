931c49e0d15f10748c69a74e8e169e5803cfa183
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-24 20:22:07 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,9 +11,9 @@ int _strcmp(char *s1, char *s1)
 {
 	int i, j, cmp;
 
-	for (i = 0; s1[i] != '0'; )
+	for (i = 0; s1[i] != '\0'; )
 		++i;
-	for (j = 0; s2[j] != '0'; )
+	for (j = 0; s2[j] != '\0'; )
 		++j;
 
 	cmp = i - j;
