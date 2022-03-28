e3acdac102b34790386602681ab4899c0a7df090
dagim93 <dagimtezera9300@gmail.com>
2022-03-28 12:16:43 -0700

    modified 3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,14 +4,13 @@
 * _strspn- calculate number of string are same in two strings
 * @s: string that is going to be checked
 * @accept: string to check.
-* @n: length of prefix substring.
 *
 * Return: unsigned int n;
 */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned i, j, n = 0;
+	unsigned int i, j, n = 0;
 
 	for (i = 0; s[i] != ' ' && s[i] != '\0'; i++)
 		for (j = 0; accept[j] != '\0'; j++)
