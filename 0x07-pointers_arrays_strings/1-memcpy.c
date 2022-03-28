27ab7ac133e6b3f3a7109445274b19cfd75753c1
dagim93 <dagimtezera9300@gmail.com>
2022-03-28 01:34:47 -0700

    add new file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _memcpy- copy block of memory form src to dest
+* @dest: address of memory to be filled
+* @src: value to be copied
+* @n: number of bytes
+*
+* Return: char dest.
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
