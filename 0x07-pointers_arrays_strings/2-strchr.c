8f396ecdba82e31360b3295ab614913a3fa5f04a
dagim93 <dagimtezera9300@gmail.com>
2022-03-28 11:12:35 -0700

    modified file 2 chrstr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strchr- find first occurence of a string.
+* @c : the string to be found.
+* @s : char string to find c.
+*
+* Return: char ot NULL.
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+			return (&s[i]);
+
+	return (NULL);
+}
