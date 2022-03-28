a55aa3f4a341bcfe3fcdfba8b6fdd4581a39ae34
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-03-28 23:34:42 +0300

    Update 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strstr - finds the character in the string.
+* @haystack: sting to be searched. 
+* @needle: sting to be searched.
+*
+* Return: char or 0.
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+      int i, j = 0;
+
+      for (i = 0; haystack[i] != '\0'; ++i)
+            if (needle[j] == haystack[i])
+                  return (&haystack[i]);
+      return (0);
+}
