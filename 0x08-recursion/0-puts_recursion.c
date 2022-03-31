d0bedf127b1312e593fc483dda4a2114f1f01aa8
dagim93 <dagimtezera9300@gmail.com>
2022-03-31 04:28:37 -0700

    modified file 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _puts_recursion- prints string input.
+* @s: the string to be printed.
+*
+* Return: none or void.
+*/
+
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+}
