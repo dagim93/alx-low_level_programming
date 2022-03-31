16f9269c8a6ded29ead19c7a0b6df7b1d63d84b6
dagim93 <dagimtezera9300@gmail.com>
2022-03-31 06:36:52 -0700

    add file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _pow_recursion- gives the power root of a number.
+* @x: the number to be powered.
+* @y: how many time to power x.
+*
+* Reuturn: int number.
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recurion(x, y - 1));
+}
