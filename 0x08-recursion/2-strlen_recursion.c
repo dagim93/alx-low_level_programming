6ab90fcd8c79f3af67301d7b2899305f31bc31da
dagim93 <dagimtezera9300@gmail.com>
2022-03-31 06:08:01 -0700

    fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,9 +7,9 @@
 * Return: number of string int.
 */
 
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
-	if (*s == NULL)
+	if (*s == '0')
 		return (0);
 	else
 		return (1 + _strlen_recursion(s + 1));
