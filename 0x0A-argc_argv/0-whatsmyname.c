59a0bfd0f1041f3551eebb077c4978f3cd180d83
dagim93 <dagimtezera9300@gmail.com>
2022-04-01 12:50:04 -0700

    add new files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main- prints program name.
+* @argv: strings that have been passed.
+*
+* Return: 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc != 0)
+		for (i = 0; *argv[i] != '/0'; i++)
+			_putchar(*argv[i]);
+
+	return (0);
+}
