8eba904f3ba2bb6483cd70ee1733408996879e92
dagim93 <dagimtezera9300@gmail.com>
2022-04-12 10:56:15 -0700

    fix style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - description of the dog.
+* @name: name of the dog.
+* @owner: owner of the dog.
+* @age: age of the dog.
+*
+*/
+
 struct dog
 {
 	char *name;
 	char *owner;
 	double age;
 };
+#endif
