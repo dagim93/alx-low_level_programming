fddca8c9bb25fe037e242eb8ba481205c086e350
dagim93 <99432287+dagim93@users.noreply.github.com>
2022-06-17 00:01:43 +0300

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,38 +1,33 @@
 #include "lists.h"
-#include <string.h>
-#include <stdio.h>
-
 /**
- * add_dnodeint_end - add a node at the end of a linked list
- * @head: The character to print
- * @n: string for the new node
- *
- * Return: new node
+ * add_dnodeint_end - Add a node in the tail
+ * @head: Pointer to direction of the head
+ * @n: The data integer
+ * Return: The direction of the tail node
  */
-
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *current;
-	dlistint_t *new_node;
+	dlistint_t *new_mem, *last_dir;
 
-	current = *head;
-	while (current && current->next != NULL)
-		current = current->next;
+	new_mem = malloc(sizeof(dlistint_t));
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-	{
-		free(new_node);
+	if (new_mem == NULL)
 		return (NULL);
+
+	new_mem->n = n;
+	new_mem->next = NULL;
+	new_mem->prev = NULL;
+	if (*head == NULL)
+	{
+		*head = new_mem;
+		return (new_mem);
 	}
-	new_node->n = n;
-	new_node->next = NULL;
-    new_node->prev = current;
+	last_dir = *head;
 
-	if (current)
-		current->next = new_node;
-	else
-		*head = new_node;
+	while (last_dir->next != NULL)
+		last_dir = last_dir->next;
+	last_dir->next = new_mem;
+	new_mem->prev = last_dir;
 
-	return (new_node);
+	return (new_mem);
 }
